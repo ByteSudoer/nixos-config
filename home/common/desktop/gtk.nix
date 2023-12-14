0d50921b6485d8c555fc39941a07c6256489c444
Soussi Mohamed Nour <88513682+ByteSudoer@users.noreply.github.com>
2023-12-14 19:06:23 +0100

    Update gtk.nix for various themes

--- home/common/desktop/gtk.nix
+++ home/common/desktop/gtk.nix
@@ -1,25 +1,44 @@
-{ pkgs,colorscheme, ... }:
-{
+{ pkgs, colorscheme, ... }:
 
+let
+  themeConfig = pkgs.lib.match colorscheme {
+    dracula = {
+      cursorTheme = {
+        package = pkgs.dracula-theme;
+        name = "Dracula";
+      };
+      theme = {
+        package = pkgs.dracula-theme;
+        name = "Dracula";
+      };
+      iconTheme = {
+        package = pkgs.dracula-icon-theme;
+        name = "Dracula";
+      };
+    };
+    gruvbox = {
+      cursorTheme = {
+        package = pkgs.capitaine-cursors-themed;
+        name = "Capitaine Cursors (Gruvbox)";
+      };
+      theme = {
+        package = pkgs.gruvbox-gtk-theme;
+        name = "Gruvbox-Dark-B";
+      };
+      iconTheme = {
+        package = pkgs.gruvbox-dark-icons-gtk;
+        name = "oomox-gruvbox-dark";
+      };
+    };
+    # Add more patterns for other color schemes
+    _ = {
+      # Default configuration or handle other color schemes
+    };
+  };
+in
+{
   gtk = {
     enable = true;
-
-    # Dracula Config
-    cursorTheme.package = pkgs.dracula-theme;
-    cursorTheme.name = "Dracula";
-    theme.package = pkgs.dracula-theme;
-    theme.name = "Dracula";
-    iconTheme.package = pkgs.dracula-icon-theme;
-    iconTheme.name = "Dracula";
-
-    #Gruvbox config
-    # cursorTheme.package = pkgs.capitaine-cursors-themed;
-    # cursorTheme.name = "Capitaine Cursors (Gruvbox)";
-    #
-    # theme.package = pkgs.gruvbox-gtk-theme;
-    # theme.name = "Gruvbox-Dark-B";
-    #
-    # iconTheme.package = pkgs.gruvbox-dark-icons-gtk;
-    # iconTheme.name = "oomox-gruvbox-dark";
+    inherit (themeConfig) cursorTheme theme iconTheme;
   };
 }

7ac3efd68587b4e5e98d3df3eef0ed9bfbe91bc6
ByteSudoeer <bytesudoer@github.com>
2023-12-14 19:44:34 +0100

    alacritty update theme

--- home/common/desktop/alacritty.nix
+++ home/common/desktop/alacritty.nix
@@ -1,11 +1,78 @@
-{ pkgs, ... }:
+{ pkgs, colorscheme,... }:
 let
   font = "JetBrains Mono";
+
+  # Define the color schemes
+  colorConfig = if colorscheme == "gruvbox" then
+  {
+    primary = {
+      background = "0x1d2021";
+      foreground = "0xd4be98";
+    };
+    normal = {
+      black = "0x32302f";
+      red = "0xea6962";
+      green = "0xa9b665";
+      yellow = "0xd8a657";
+      blue = "0x7daea3";
+      magenta = "0xd3869b";
+      cyan = "0x89b482";
+      white = "0xd4be98";
+    };
+    bright = {
+      black = "0x32302f";
+      red = "0xea6962";
+      green = "0xa9b665";
+      yellow = "0xd8a657";
+      blue = "0x7daea3";
+      magenta = "0xd3869b";
+      cyan = "0x89b482";
+      white = "0xd4be98";
+    };
+  }
+
+else if colorscheme == "dracula" then
+{
+    primary = {
+      background = "0x282a36";
+      foreground = "0xf8f8f2";
+    };
+    normal = {
+      black = "0x000000";
+      red = "0xff5555";
+      green = "0x50fa7b";
+      yellow = "0xf1fa8c";
+      blue = "0xcaa9fa";
+      magenta = "0xff79c6";
+      cyan = "0x8be9fd";
+      white = "0xbfbfbf";
+    };
+    bright = {
+      black = "0x575b70";
+      red = "0xff6e67";
+      green = "0x5af78e";
+      yellow = "0xf4f99d";
+      blue = "0xcaa9fa";
+      magenta = "0xff92d0";
+      cyan = "0x9aedfe";
+      white = "0xe6e6e6";
+    };
+  }
+  else
+  {
+
+  };
+
+
 in
 {
-  #Configure Alacritty
+  # Configure Alacritty
   programs.alacritty = {
     enable = true;
+    package = pkgs.alacritty;
+    settings = {
+      # ... other settings ...
+      enable = true;
     package = pkgs.alacritty;
     settings = {
       cursor = {
@@ -47,137 +114,9 @@ in
           style = "Bold Italic";
         };
       };
-      colors = {
-
-        # #### Gruvbox
-        # primary = {
-        #   background = "0x1d2021";
-        #   foreground = "0xd4be98";
-        # };
-        #
-        # normal = {
-        #   black = "0x32302f";
-        #   red = "0xea6962";
-        #   green = "0xa9b665";
-        #   yellow = "0xd8a657";
-        #   blue = "0x7daea3";
-        #   magenta = "0xd3869b";
-        #   cyan = "0x89b482";
-        #   white = "0xd4be98";
-        # };
-        # bright = {
-        #   black = "0x32302f";
-        #   red = "0xea6962";
-        #   green = "0xa9b665";
-        #   yellow = "0xd8a657";
-        #   blue = "0x7daea3";
-        #   magenta = "0xd3869b";
-        #   cyan = "0x89b482";
-        #   white = "0xd4be98";
-        # };
-        #### Dracula
-        primary = {
-          background = "0x282a36";
-          foreground = "0xf8f8f2";
-        };
-        normal = {
-          black = "0x000000";
-          red = "0xff5555";
-          green = "0x50fa7b";
-          yellow = "0xf1fa8c";
-          blue = "0xcaa9fa";
-          magenta = "0xff79c6";
-          cyan = "0x8be9fd";
-          white = "0xbfbfbf";
-        };
-        bright = {
-          black = "0x575b70";
-          red = "0xff6e67";
-          green = "0x5af78e";
-          yellow = "0xf4f99d";
-          blue = "0xcaa9fa";
-          magenta = "0xff92d0";
-          cyan = "0x9aedfe";
-          white = "0xe6e6e6";
-        };
-
-      };
     };
-  };
-}
-{ pkgs, ... }:
 
-let
-  font = "JetBrains Mono";
-
-  # Define the color schemes
-  gruvboxColors = {
-    primary = {
-      background = "0x1d2021";
-      foreground = "0xd4be98";
-    };
-    normal = {
-      black = "0x32302f";
-      red = "0xea6962";
-      green = "0xa9b665";
-      yellow = "0xd8a657";
-      blue = "0x7daea3";
-      magenta = "0xd3869b";
-      cyan = "0x89b482";
-      white = "0xd4be98";
-    };
-    bright = {
-      black = "0x32302f";
-      red = "0xea6962";
-      green = "0xa9b665";
-      yellow = "0xd8a657";
-      blue = "0x7daea3";
-      magenta = "0xd3869b";
-      cyan = "0x89b482";
-      white = "0xd4be98";
-    };
-  };
-
-  draculaColors = {
-    primary = {
-      background = "0x282a36";
-      foreground = "0xf8f8f2";
-    };
-    normal = {
-      black = "0x000000";
-      red = "0xff5555";
-      green = "0x50fa7b";
-      yellow = "0xf1fa8c";
-      blue = "0xcaa9fa";
-      magenta = "0xff79c6";
-      cyan = "0x8be9fd";
-      white = "0xbfbfbf";
-    };
-    bright = {
-      black = "0x575b70";
-      red = "0xff6e67";
-      green = "0x5af78e";
-      yellow = "0xf4f99d";
-      blue = "0xcaa9fa";
-      magenta = "0xff92d0";
-      cyan = "0x9aedfe";
-      white = "0xe6e6e6";
-    };
-  };
-
-  # Select the colors based on the theme
-  selectedColors = if pkgs.lib.strings.isInfixOf "dracula" "gruvbox" then draculaColors else gruvboxColors;
-
-in
-{
-  # Configure Alacritty
-  programs.alacritty = {
-    enable = true;
-    package = pkgs.alacritty;
-    settings = {
-      # ... other settings ...
-
-      colors = selectedColors;
+      colors = colorConfig;
     };
   };
 }

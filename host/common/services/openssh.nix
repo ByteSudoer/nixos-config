399039f80b19e51595b6a5ad2ed5bd91c18210ca
ByteSudoer <ByteSudoer@github.com>
2023-12-16 08:27:54 +0100

    update ssh

--- host/common/services/openssh.nix
+++ host/common/services/openssh.nix
@@ -5,10 +5,6 @@
       PasswordAuthentication = false;
       PermitRootLogin = lib.mkDefault "no";
     };
-    authorizedKeys.keys = [
-    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj/FQIaOA6fDCejH8hI1DU7X0zfL/x491kqRECCePRMNWLA+hrSMvpKm6EwDw3bQ82k3F+GAdpMFekYDAeuXpNUcgngDbRYe/RmfcRjaEXzqt13vzsAbSuybIRWp2ziQfbClPBXlCLT8P97jZbZ8/RCmiROeFvWk9s/XGF/3w7p2yBH4+vffHxGAuCYwIFaDyuoaJ/WUOShPsH+dRBeIloqBKbq0ZBLKhFIDEuD0p0XG7J7XSaniK85WQ1dkPNv0Wi47ZXZA9UCCzvFlOPAi3KUI/5u2u9GL2klmX0WGPtV0iLP8p60SovDLmFK65R4zoDVTQyuGb3Cigl+hbq1NuJ/7zZ09bbMwHAOXQBLQmi/HO6fgt+MXK2Ong1SeQO4Fdxjm1iWDHP1VCoij3FSeK6WWSvXyFzf8g5SJlaQt+I2Hel0DHVKYnlbYf6/nrroV+8UGI1AP/Hiiuppashx5Iu5jTvSlwQSL74XOMPoAN5ukAmbzAYuQn+xJ6vRhjIXAc= Nix VM Key
-"
-    ];
   };
 
   programs.ssh.startAgent = true;
--- host/common/users/bytesudoer/default.nix
+++ host/common/users/bytesudoer/default.nix
@@ -24,10 +24,10 @@ in
         "lxd"
       ];
 
-    # openssh.authorizedKeys.keys = [
-    #   "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIB9bIEMgZVBCDxBWQ4m4hQP6ZZp0P3TfzjzcgUOdbYDLAAAABHNzaDo= YK5C"
-    #   "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIBC8cs1B64XqEswY5pART6yERbjUMB7RdQdT38dgkZT6AAAABHNzaDo= YK5"
-    # ];
+    openssh.authorizedKeys.keys = [
+       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj/FQIaOA6fDCejH8hI1DU7X0zfL/x491kqRECCePRMNWLA+hrSMvpKm6EwDw3bQ82k3F+GAdpMFekYDAeuXpNUcgngDbRYe/RmfcRjaEXzqt13vzsAbSuybIRWp2ziQfbClPBXlCLT8P97jZbZ8/RCmiROeFvWk9s/XGF/3w7p2yBH4+vffHxGAuCYwIFaDyuoaJ/WUOShPsH+dRBeIloqBKbq0ZBLKhFIDEuD0p0XG7J7XSaniK85WQ1dkPNv0Wi47ZXZA9UCCzvFlOPAi3KUI/5u2u9GL2klmX0WGPtV0iLP8p60SovDLmFK65R4zoDVTQyuGb3Cigl+hbq1NuJ/7zZ09bbMwHAOXQBLQmi/HO6fgt+MXK2Ong1SeQO4Fdxjm1iWDHP1VCoij3FSeK6WWSvXyFzf8g5SJlaQt+I2Hel0DHVKYnlbYf6/nrroV+8UGI1AP/Hiiuppashx5Iu5jTvSlwQSL74XOMPoAN5ukAmbzAYuQn+xJ6vRhjIXAc= Nix VM Key
+"
+    ];
 
     packages = [ pkgs.home-manager ];
   };

6f60c71163687aba05f373072d19df8ad4467a1a
ByteSudoer <ByteSudoer@users.noreply.github.com>
2023-12-28 14:19:33 +0100

    xfce config

--- home/common/desktop/xfce/default.nix
+++ home/common/desktop/xfce/default.nix
@@ -1,31 +1,31 @@
 { pkgs, ... }:
 {
 
-  home.packages = with pkgs;[
-    xfce.xfce4-whiskermenu-plugin
-    xfce.xfce4-weather-plugin
-    xfce.xfce4-volumed-pulse
-    xfce.xfce4-systemload-plugin
-    xfce.xfce4-settings
-    xfce.xfce4-session
-    xfce.xfce4-pulseaudio-plugin
-    xfce.xfce4-power-manager
-    xfce.xfce4-panel-profiles
-    xfce.xfce4-panel
-    xfce.xfce4-netload-plugin
-    xfce.xfce4-datetime-plugin
-    xfce.xfce4-battery-plugin
-    xorg.xdpyinfo
-    xarchiver
-    xclip
-
-    libsForQt5.qt5.qtquickcontrols2
-    libsForQt5.qt5.qtgraphicaleffects
-    lxappearance
-  ];
+  # home.packages = with pkgs;[
+  #   xfce.xfce4-whiskermenu-plugin
+  #   xfce.xfce4-weather-plugin
+  #   xfce.xfce4-volumed-pulse
+  #   xfce.xfce4-systemload-plugin
+  #   xfce.xfce4-settings
+  #   xfce.xfce4-session
+  #   xfce.xfce4-pulseaudio-plugin
+  #   xfce.xfce4-power-manager
+  #   xfce.xfce4-panel-profiles
+  #   xfce.xfce4-panel
+  #   xfce.xfce4-netload-plugin
+  #   xfce.xfce4-datetime-plugin
+  #   xfce.xfce4-battery-plugin
+  #   xorg.xdpyinfo
+  #   xarchiver
+  #   xclip
+  #
+  #   libsForQt5.qt5.qtquickcontrols2
+  #   libsForQt5.qt5.qtgraphicaleffects
+  #   lxappearance
+  # ];
 
   xdg.configFile.xfce4 = {
-    source = ../../../config/xfce;
+    source = ../../../../config/xfce4;
     recursive = true;
   };
 }
--- host/common/desktop/xfce.nix
+++ host/common/desktop/xfce.nix
@@ -1,4 +1,4 @@
-_: {
+{ pkgs, ... }: {
 
   services.xserver = {
     enable = true;
@@ -8,4 +8,27 @@ _: {
     };
     displayManager.defaultSession = "xfce";
   };
+  environment.systemPackages = with pkgs;[
+
+    xfce.xfce4-whiskermenu-plugin
+    xfce.xfce4-weather-plugin
+    xfce.xfce4-volumed-pulse
+    xfce.xfce4-systemload-plugin
+    xfce.xfce4-settings
+    xfce.xfce4-session
+    xfce.xfce4-pulseaudio-plugin
+    xfce.xfce4-power-manager
+    xfce.xfce4-panel-profiles
+    xfce.xfce4-panel
+    xfce.xfce4-netload-plugin
+    xfce.xfce4-datetime-plugin
+    xfce.xfce4-battery-plugin
+    xorg.xdpyinfo
+    xarchiver
+    xclip
+
+    libsForQt5.qt5.qtquickcontrols2
+    libsForQt5.qt5.qtgraphicaleffects
+    lxappearance
+  ];
 }

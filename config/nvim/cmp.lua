0374eb453ef8dce3a39283dc5d509b33ae5484be
ByteSudoer <ByteSudoer@users.noreply.github.com>
2023-12-29 08:56:06 +0100

    formatted lua

--- config/nvim/cmp.lua
+++ config/nvim/cmp.lua
@@ -2,81 +2,81 @@ local cmp = require("cmp")
 local lspkind = require("lspkind")
 
 function check_back_space()
-	local col = vim.fn.col(".") - 1
-	if col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
-		return true
-	else
-		return false
-	end
+  local col = vim.fn.col(".") - 1
+  if col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
+    return true
+  else
+    return false
+  end
 end
 
 cmp.setup({
-	formatting = {
-		format = lspkind.cmp_format({
-			mode = "symbol_text", -- show only symbol annotations
-			maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-		}),
-	},
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = "symbol_text", -- show only symbol annotations
+      maxwidth = 50,     -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+    }),
+  },
 
-	mapping = {
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item(select_opts)
-			elseif check_back_space() then
-				fallback()
-			else
-				cmp.complete()
-			end
-		end, { "i", "s" }),
+  mapping = {
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item(select_opts)
+      elseif check_back_space() then
+        fallback()
+      else
+        cmp.complete()
+      end
+    end, { "i", "s" }),
 
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item(select_opts)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-		["<C-Space"] = cmp.mapping.complete(),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item(select_opts)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<C-Space"] = cmp.mapping.complete(),
 
-		["<CR>"] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Insert,
-			select = true,
-		}),
-	},
+    ["<CR>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    }),
+  },
 
-	snippet = {
-		expand = function(args)
-			require("luasnip").lsp_expand(args.body)
-		end,
-	},
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
 
-	sources = {
-		{ name = "nvim_lsp" },
-		{ name = "treesitter" },
-		{ name = "buffer" },
-		{ name = "luasnip" },
-		{ name = "nvim_lua" },
-		{ name = "path" },
-		{ name = "vsnip" },
-		{ name = "crates" },
-		{ name = "crates" },
-		{ name = "cmdline" },
-	},
-	window = {
-		completion = cmp.config.window.bordered(),
-		documentation = {
-			border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-			--   winhighlight = "NormalFloat:NormalFloat,FloatBorder:TelescopeBorder",
-		},
-	},
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "treesitter" },
+    { name = "buffer" },
+    { name = "luasnip" },
+    { name = "nvim_lua" },
+    { name = "path" },
+    { name = "vsnip" },
+    { name = "crates" },
+    { name = "crates" },
+    { name = "cmdline" },
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = {
+      border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+      --   winhighlight = "NormalFloat:NormalFloat,FloatBorder:TelescopeBorder",
+    },
+  },
 })
 cmp.setup.cmdline(":", {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = cmp.config.sources({
-		{ name = "path" },
-	}, {
-		{ name = "cmdline" },
-	}),
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = "path" },
+  }, {
+    { name = "cmdline" },
+  }),
 })
 
 vim.cmd([[

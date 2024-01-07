c12915e6273414d1492f8092d1fed340155e395a
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-01-07 18:22:50 +0100

    change bat theme with global colorscheme

--- home/common/shell/bat.nix
+++ home/common/shell/bat.nix
@@ -1,11 +1,14 @@
-{ pkgs, ... }:
+{ pkgs, colorscheme, ... }:
+let
+  color = if colorscheme == "dracula" then "Dracula" else if colorscheme == "gruvbox" then "gruvbox-dark" else "Dracula";
+in
 {
   # Configure Bat
   programs.bat = {
     enable = true;
     config = {
       pager = "less -FR";
-      theme = "gruvbox-dark";
+      theme = "${color}";
     };
     extraPackages = with pkgs.bat-extras; [ batdiff batman batgrep batwatch ];
   };
--- home/common/shell/default.nix
+++ home/common/shell/default.nix
@@ -1,4 +1,5 @@
 { pkgs
+, coloescheme
 , ...
 }: {
   imports = [

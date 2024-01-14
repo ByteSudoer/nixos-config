a6766c6e4dac56d829ab8768162e08d37dad7f28
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-01-14 14:28:13 +0100

    update nerdfetch hash

--- pkgs/nerdfetch/default.nix
+++ pkgs/nerdfetch/default.nix
@@ -1,14 +1,18 @@
 { lib
 , stdenv
-, fetchgit
+, fetchFromGitea
 }:
 stdenv.mkDerivation rec {
   pname = "NerdFetch";
   version = "v5.0.2";
-  src = fetchgit {
-    url = "https://codeberg.org/thatonecalculator/NerdFetch.git";
-    hash = "sha256-olDOu+dxn6JEnolcYgodq98y/ek+aGdDKT5cUAIBums=";
-  };
+  src = fetchFromGitea
+    {
+      domain = "codeberg.org";
+      owner = "thatonecalculator";
+      repo = "NerdFetch";
+      rev = "228ad50b770a5bc02ad073dcfdbb687e912aa6de";
+      hash = "sha256-2Adxh0UlmAovYPk3R4yYDpW5RBZgPSNkEj+C+9DaiLE=";
+    };
   dontUnpack = true;
   dontConfigure = true;
   dontBuild = true;

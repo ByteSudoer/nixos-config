f7ea5bb0856a734152ece33441df4fc9e956bd14
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-01-16 16:50:36 +0100

    media player for spotify

--- pkgs/scripts/lock.nix
+++ pkgs/scripts/lock.nix
@@ -1,31 +1,7 @@
 { pkgs, ... }:
+let
+  content = builtins.readFile ./mediaplayer.py;
+in
 pkgs.writeShellScriptBin "lock" ''
-  #!/usr/bin/env bash 
-
-    image=/home/bytesudoer/Pictures/Wallpapers/dracula/kernel.png
-    if [ "$WAYLAND_DISPLAY" ]; then
-      lock_command=swaylock
-    else
-      lock_command=betterlockscreen -l
-    fi
-
-
-    amixer set Master mute
-  ## Test if spotify is running and pause song
-    if [[ $(pgrep spotify) ]];then
-      sp pause
-    fi
-
-  ## Test if clementine is running and pause song
-    if [[ $(pgrep clementine) ]];then
-      clementine --pause
-    fi
-  ##### Run Lock Command
-  $lock_command
-  #
-
-  ## When testing use the --no-verify option
-  ##### Use i3lock
-  # i3lock -i "$image" --fill --pointer=win --show-failed-attempts 
-  amixer set Master unmute
+  echo ${content}
 ''

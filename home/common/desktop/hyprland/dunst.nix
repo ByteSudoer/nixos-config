bb12d11a743633c579366f3756f0c807a6a438fa
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-01-17 19:52:00 +0100

    dunst config

--- home/common/desktop/hyprland/dunst.nix
+++ home/common/desktop/hyprland/dunst.nix
@@ -1,7 +1,14 @@
-_:
+{ colorscheme, desktop, lib, ... }:
+let
+  isHyprland = desktop: lib.toUpper desktop == "HYPRLAND";
+  config_file = ../../../../config/dunst/dunst_${colorscheme};
+in
 {
   services.dunst = {
-    enable = true;
+    enable = isHyprland desktop;
+  };
 
+  xdg.configFile."dunst/dunstrc" = {
+    source = config_file;
   };
 }

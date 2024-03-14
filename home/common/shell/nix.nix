3fa0698315d6cddd657e5161607a8733db477178
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-03-14 12:28:18 +0100

    update nix config added more mirrors

--- home/common/shell/nix.nix
+++ home/common/shell/nix.nix
@@ -13,8 +13,20 @@
       # warn-dirty = false;
       auto-optimise-store = true;
       builders-use-substitutes = true;
-      experimental-features = [ "nix-command" "flakes" ];
       trusted-users = [ "@wheel" ];
+
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      substituters = [
+        "https://hyprland.cachix.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
   };
 }

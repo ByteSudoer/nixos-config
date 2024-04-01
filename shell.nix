820f3a9b3050269d6806ea928290e1c8a120361f
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-04-01 03:24:48 +0100

    change shell name

--- shell.nix
+++ shell.nix
@@ -1,5 +1,6 @@
 { pkgs ? (import ./nixpkgs.nix) { } }: {
   default = pkgs.mkShell {
+    name = "dotfiles";
     # Enable experimental features without having to specify the argument
     NIX_CONFIG = "experimental-features = nix-command flakes";
     nativeBuildInputs = with pkgs; [

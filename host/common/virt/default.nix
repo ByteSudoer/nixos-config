6e3998dfd6fa6e85a4aeb0366f376d05f4baa7fd
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-05-15 19:08:20 +0100

    remove vmware

--- host/common/virt/default.nix
+++ host/common/virt/default.nix
@@ -3,6 +3,6 @@
     ./docker.nix
   ] # Include quickemu if a desktop is defined
   ++ lib.optional (builtins.isString desktop) ./quickemu.nix
-  ++ lib.optional (builtins.isString desktop) ./vmware.nix
+  # ++ lib.optional (builtins.isString desktop) ./vmware.nix
   ++ lib.optional (builtins.isString extra) ./virt-manager.nix;
 }

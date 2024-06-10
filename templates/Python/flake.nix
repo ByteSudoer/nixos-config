79c0c7522c3099f308e76158cf2233c1b12b3d0f
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-06-10 21:41:43 +0100

    update template input url from 23.11 -> 24.05

--- templates/Python/flake.nix
+++ templates/Python/flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Nix Shell For Python development";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 

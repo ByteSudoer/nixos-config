6a6453839adfe34439db37193f7b15c11f799542
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-07-12 17:26:26 +0100

    config awesome

---
+++ config/awesome/autostart.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+
+}
+run nm-applet &
+#run caffeine
+# run variety
+# run xfce4-power-manager
+# run blueberry-tray
+run numlockx on &
+# run volumeicon
+run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+#run /usr/lib/xfce4/notifyd/xfce4-notifyd & 
+run fehrandom &
+run wallpapers.sh &
+
+# run defLayout
+# run conky -c $HOME/.config/awesome/system-overview
+#you can set wallpapers in themes as well
+# feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
+#run applications from startup
+#run firefox
+#run atom
+#run dropbox
+#run insync start
+#run spotify
+#run ckb-next -b
+run discord --start-minimized &
+run ferdium &
+# run birdtray &
+#run telegram-desktop
+run defaultLayout &
+# Launch polybar
+# "$HOME"/.config/polybar/launch.sh &
+# run dropbox &
+# restart_window_manager &

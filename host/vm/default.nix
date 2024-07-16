2740f54143357ce24e430102cf4193c94ac330d2
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-07-16 12:24:11 +0100

    fix formatting

--- host/vm/default.nix
+++ host/vm/default.nix
@@ -3,7 +3,7 @@
   imports = [
     (import ./boot.nix { })
     (import ./hardware.nix { inherit lib modulesPath; })
-    (import ./disks.nix)
+    (import ./disks.nix { })
   ];
 
   services.spice-vdagentd.enable = true;

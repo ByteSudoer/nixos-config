f3cdf9a7ef6c14259bdb778f62daed641e3b8f24
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-07-17 16:42:48 +0100

    bashtop config

--- home/common/shell/btop.nix
+++ home/common/shell/btop.nix
@@ -1,7 +1,20 @@
-{ pkgs, ... }:
+{ pkgs, colorscheme, desktop, ... }:
+let
+  palette =
+    if colorscheme == "dracula"
+    then "dracula"
+    else if colorscheme == "gruvbox"
+    then "gruvbox_material_dark"
+    else "default";
+  isDesktop = builtins.isString desktop;
+in
 {
   programs.btop = {
     enable = true;
     package = pkgs.btop;
+    settings = {
+      color_theme = "${palette}";
+      update_ms = 1500;
+    };
   };
 }

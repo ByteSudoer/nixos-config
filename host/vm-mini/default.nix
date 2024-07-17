a09c4edba421f956121fc31959bae8f63fb36cfa
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-07-17 11:33:46 +0100

    Disko config for VM-mini host: ext4 and btrfs

--- host/vm-mini/default.nix
+++ host/vm-mini/default.nix
@@ -1,8 +1,9 @@
-{ lib, modulesPath, platform, ... }:
+{ lib, modulesPath, platform, fs, ... }:
 {
   imports = [
     (import ./boot.nix { })
     (import ./hardware.nix { inherit lib modulesPath; })
+    import ./disk-${fs}.nix
   ];
 
   nixpkgs.hostPlatform = lib.mkDefault "${platform}";
---
+++ host/vm-mini/disk-ext4.nix
@@ -0,0 +1,36 @@
+{ disks ? [ "/dev/vda" ], ... }: {
+  disko.devices = {
+    disk = {
+      my-disk = {
+        type = "disk";
+        device = builtins.elemAt disks 0;
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02";
+            };
+            ESP = {
+              type = "EF00";
+              size = "512M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}

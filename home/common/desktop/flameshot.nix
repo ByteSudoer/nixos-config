542f47ba307463f1bc9c281cab38b02ab5972be3
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-07-17 16:27:40 +0100

    flameshot config

--- home/common/desktop/flameshot.nix
+++ home/common/desktop/flameshot.nix
@@ -1,7 +1,17 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   services.flameshot = {
     enable = true;
     package = pkgs.flameshot;
+
+    # Configuration to be saved in an INI file
+    settings = {
+      General = {
+        savePath = "${config.home.homeDirectory}/Pictures/screenshots";
+        saveAsFileExtension = ".png";
+        disabledTrayIcon = false;
+        useJpgForClipboard = false;
+      };
+    };
   };
 }

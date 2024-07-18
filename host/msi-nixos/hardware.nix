97d0ac8e9ee19054c4a2ec364b4a6d680496e314
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-07-18 10:33:43 +0100

    configure hardware for host

--- host/msi-nixos/hardware.nix
+++ host/msi-nixos/hardware.nix
@@ -8,22 +8,9 @@
 
   imports =
     [
-      (modulesPath + "/profiles/qemu-guest.nix")
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
-  fileSystems."/" =
-    {
-      device = "/dev/disk/by-label/nixos";
-      fsType = "ext4";
-    };
 
-  fileSystems."/boot" =
-    {
-      device = "/dev/disk/by-label/boot";
-      fsType = "vfat";
-    };
-
-  swapDevices =
-    [{ device = "/dev/disk/by-label/swap"; }];
 
   networking.useDHCP = lib.mkDefault true;
 

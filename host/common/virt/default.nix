a9e1fbb9ef5693faf6a66c2818b2828a06ed6575
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-07-28 11:39:48 +0100

    vmware

--- host/common/virt/default.nix
+++ host/common/virt/default.nix
@@ -3,6 +3,6 @@
   imports =
     [ ./docker.nix ] # Include quickemu if a desktop is defined
     ++ lib.optional (builtins.isString extra) ./quickemu.nix
-    # ++ lib.optional (builtins.isString extra) ./vmware.nix
+    ++ lib.optional (builtins.isString extra) ./vmware.nix
     ++ lib.optional (builtins.isString extra) ./virt-manager.nix;
 }

c8b22ff126ebef90e1eb1d3261e70085ab804016
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-07-29 19:50:54 +0100

    new fingerprint reader driver: not working yet

--- host/e16-nixos/hardware.nix
+++ host/e16-nixos/hardware.nix
@@ -22,7 +22,20 @@
   networking.useDHCP = lib.mkDefault true;
 
   # Fingerprint reader: login and unlock with fingerprint (if you add one with `fprintd-enroll`)
-  services.fprintd.enable = true;
+  # Start the service at boot
+  systemd.services.fprintd = {
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.Type = "simple";
+  };
+  services = {
+    fprintd = {
+      enable = true;
+      tod.enable = true;
+      tod.driver = pkgs.libfprint-2-tod1-goodix; # Goodix driver module
+      # tod.driver = pkgs.libfprint-2-tod1-vfs0090; # driver for 2016 ThinkPads
+      # tod.driver = pkgs.libfprint-2-tod1-goodix-550a; # Goodix 550a driver (from Lenovo)
+    };
+  };
 
   # powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
   hardware = {

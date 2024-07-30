adc925d2c641b5de9990e65a2220f79121f742e3
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-07-30 11:00:11 +0100

    update res

--- pkgs/scripts/defaultLayout.nix
+++ pkgs/scripts/defaultLayout.nix
@@ -8,14 +8,14 @@ pkgs.writeShellScriptBin "defaultLayout" ''
 
   if [ "$virt" == "kvm" ] || [ "$virt" == "oracle" ];then
     primary="Virtual-1"
-    xrandr --output  "$primary" --primary --mode 1920x1080 --pos 0x0 --rotate normal 
+    xrandr --output  "$primary" --primary --mode 1920x1200 --pos 0x0 --rotate normal 
   else
     primary="eDP-1"
     externel="HDMI-1"
     if xrandr | grep -q "$externel connected"; then
-     xrandr --output  "$primary" --mode 1920x1080 --pos 1920x0 --rotate normal --output "$externel" --primary --mode 1920x1080 --pos 0x0 --right-of "$primary" --rotate normal
+     xrandr --output  "$primary" --mode 1920x1200 --pos 1920x0 --rotate normal --output "$externel" --primary --mode 1920x1200 --pos 0x0 --right-of "$primary" --rotate normal
     else
-     xrandr --output  "$primary" --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output "$externel" --off --output HDMI-1-1 --off
+     xrandr --output  "$primary" --primary --mode 1920x1200 --pos 0x0 --rotate normal --output DP-1 --off --output "$externel" --off --output HDMI-1-1 --off
     fi
   fi
 

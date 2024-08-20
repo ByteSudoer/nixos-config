2c7986c1029bd378ba50e9740cf6f263c561fb27
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-08-20 20:16:16 +0100

    vmware unstable -> stable

--- host/common/virt/vmware.nix
+++ host/common/virt/vmware.nix
@@ -1,14 +1,14 @@
-{ pkgs
-, extra
-, lib
-, ...
+{
+  pkgs,
+  extra,
+  lib,
+  ...
 }:
 {
   virtualisation.vmware = {
     host.enable = lib.isString extra;
-    host.package = pkgs.unstable.vmware-workstation;
+    host.package = pkgs.vmware-workstation;
   };
-
 
   environment.systemPackages = with pkgs; [ linux-wifi-hotspot ];
 

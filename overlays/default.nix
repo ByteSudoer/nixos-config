efa3abab7f81b52365604f64d3ae4c71c4ec872e
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-09-16 10:27:08 +0100

    Added ciscoPacketTracer theme overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,16 +1,38 @@
 # This file defines overlays
-{ inputs, colorscheme, ... }:
+{
+  inputs,
+  colorscheme,
+  ...
+}:
 {
   # This one brings our custom packages from the 'pkgs' directory
-  additions = final: _prev: import ../pkgs { pkgs = final;inherit colorscheme; };
+  additions =
+    final: _prev:
+    import ../pkgs {
+      pkgs = final;
+      inherit colorscheme;
+    };
 
   # This one contains whatever you want to overlay
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
-  modifications = _final: _prev: {
-    # example = prev.example.overrideAttrs (oldAttrs: rec {
-    # ...
-    # });
+
+  modifications = _final: prev: {
+    ciscoPacketTracer8 = prev.ciscoPacketTracer8.overrideAttrs (_oldAttrs: rec {
+      desktopItems = [
+        (prev.makeDesktopItem {
+          name = "cisco-pt8.desktop";
+          desktopName = "Cisco Packet Tracer 8";
+          icon = "ciscoPacketTracer8";
+          exec = "GTK_THEME=Adwaita:light packettracer8 %f";
+          mimeTypes = [
+            "application/x-pkt"
+            "application/x-pka"
+            "application/x-pkz"
+          ];
+        })
+      ];
+    });
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will

d1c94a725a42d17f691bf64d1277f429b5943d34
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-09-23 23:33:18 +0100

    Update PacketTracer overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,7 +1,8 @@
 # This file defines overlays
-{ inputs
-, colorscheme
-, ...
+{
+  inputs,
+  colorscheme,
+  ...
 }:
 {
   # This one brings our custom packages from the 'pkgs' directory
@@ -17,21 +18,21 @@
   # https://nixos.wiki/wiki/Overlays
 
   modifications = _final: prev: {
-    ciscoPacketTracer8 = prev.ciscoPacketTracer8.overrideAttrs (_oldAttrs: rec {
-      desktopItems = [
-        (prev.makeDesktopItem {
-          name = "cisco-pt8.desktop";
-          desktopName = "Cisco Packet Tracer 8";
-          icon = "ciscoPacketTracer8";
-          exec = "GTK_THEME=Adwaita:light packettracer8 %f";
-          mimeTypes = [
-            "application/x-pkt"
-            "application/x-pka"
-            "application/x-pkz"
-          ];
-        })
-      ];
-    });
+    # ciscoPacketTracer8 = prev.ciscoPacketTracer8.overrideAttrs (_oldAttrs: rec {
+    #   desktopItems = [
+    #     (prev.makeDesktopItem {
+    #       name = "cisco-pt8.desktop";
+    #       desktopName = "Cisco Packet Tracer 8";
+    #       icon = "ciscoPacketTracer8";
+    #       exec = "GTK_THEME=Adwaita:light packettracer8 %f";
+    #       mimeTypes = [
+    #         "application/x-pkt"
+    #         "application/x-pka"
+    #         "application/x-pkz"
+    #       ];
+    #     })
+    #   ];
+    # });
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will

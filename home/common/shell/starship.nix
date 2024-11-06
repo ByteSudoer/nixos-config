01b17b050d1c3e787a2da9d38b7bba30804cae46
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-11-06 17:42:59 +0100

    update starship config

--- home/common/shell/starship.nix
+++ home/common/shell/starship.nix
@@ -8,25 +8,51 @@ _: {
     settings = {
       add_newline = false;
       character = {
-        success_symbol = "[➜](bold green)";
+        format = "$symbol ";
+        vicmd_symbol = "[❮](bold green)";
+        disabled = false;
+        success_symbol = "[➜](bold green) ";
         error_symbol = "[✗](bold red) ";
       };
+      cmd_duration = {
+        min_time = 2000;
+        format = "⏱ [$duration]($style) ";
+        style = "yellow bold";
+        show_milliseconds = false;
+        disabled = false;
+        show_notifications = false;
+        min_time_to_notify = 45000;
+      };
       directory = {
-        truncation_length = 5;
+        disabled = false;
+        fish_style_pwd_dir_length = 0;
         format = "[$path]($style)[$lock_symbol]($lock_style) ";
+        home_symbol = "~";
+        read_only_style = "red";
+        repo_root_format = "[$before_root_path]($style)[$repo_root]($repo_root_style)[$path]($style)[$read_only]($read_only_style) ";
+        style = "cyan bold bg:0xDA627D";
+        truncate_to_repo = true;
+        truncation_length = 5;
+        truncation_symbol = "…/";
+        use_logical_path = true;
+        use_os_path_sep = true;
+        substitutions = {
+          "Downloads" = " ";
+          "Music" = " ";
+          "Pictures" = " ";
+
+        };
       };
       hostname = {
-        ssh_only = true;
-        format = "[ $ssh_symbol ] (bold blue) on [ $hostname ] (bold red) ";
+        ssh_only = false;
+        format = "[$ssh_symbol](blue dimmed bold)[$hostname]($style) ";
+        trim_at = ".";
         disabled = false;
       };
       username = {
         style_user = "bold dimmed blue";
         disabled = false;
         show_always = true;
-      };
-      cmd_duration = {
-        format = " took [$duration](bold yellow)";
       };
       package = {
         disabled = true;

ba3cf75fe7f1d831b34777e57e19f7ba10d1df73
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-12-05 17:20:03 +0100

    remove duplicate items

--- host/common/desktop/plasma.nix
+++ host/common/desktop/plasma.nix
@@ -3,7 +3,5 @@ _: {
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
-
 }

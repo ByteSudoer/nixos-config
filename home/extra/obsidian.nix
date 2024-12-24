d1615a30c550c9b997d030490ff2918735dd02f2
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-12-24 17:39:05 +0100

    Systemd service for obsidian github sync

--- home/extra/obsidian.nix
+++ home/extra/obsidian.nix
@@ -1,4 +1,46 @@
 { pkgs, ... }:
+let
+  minutes = "15";
+  scheduleExpression = "*:0/${minutes}";
+  gitSyncObsidian = pkgs.writeScriptBin "git-sync-obsidian-service" ''
+    #!/usr/bin/env bash
+
+    VAULT_DIRECTORY="$HOME/Documents/Notes"
+    pushd $VAULT_DIRECTORY
+    git add .
+    git commit -m "$(date '+%Y-%m-%d %H:%M:%S')" || exit 0
+    popd
+
+  '';
+in
 {
-  home.packages = with pkgs; [ obsidian ];
+  home.packages = with pkgs; [
+    obsidian
+    gitSyncObsidian
+  ];
+
+  # Declaration of a systemd service
+  systemd.user.services.git-sync-obsidian = {
+    Unit = {
+      Description = "Sync Obsifdian Vault to private GitHub Repository";
+      Wants = "git-sync-obsidian.timer";
+    };
+    Service = {
+      ExecStart = "${gitSyncObsidian}/bin/git-sync-obsidian-service";
+      Type = "simple";
+    };
+  };
+
+  systemd.user.timers.git-sync-obsidian = {
+    Unit = {
+      Description = "Run Git Sync for Obsidian Vault every";
+    };
+    Timer = {
+      OnCalendar = "${scheduleExpression}";
+    };
+    Install = {
+      WantedBy = [ "timers.target" ];
+    };
+  };
+
 }

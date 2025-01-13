d4661c8550f534f527d32b24d3d27d80a12980ed
ByteSudoer <ByteSudoer@users.noreply.github.com>
2025-01-13 22:50:25 +0100

    Remove auto suspend for output devices

--- host/common/services/pipewire.nix
+++ host/common/services/pipewire.nix
@@ -4,18 +4,47 @@
   security.rtkit.enable = true;
   services.pipewire = {
     audio.enable = true;
+    socketActivation = true;
     enable = true;
     alsa = {
       enable = true;
       support32Bit = true;
     };
     pulse.enable = true;
+    wireplumber = {
+      enable = true;
+      extraConfig = {
+        "99-disable-suspend" = {
+          "monitor.alsa.rules" = [
+            {
+              matches = [
+                {
+                  "node.name" = "~alsa_output.*";
+                }
+              ];
+              actions = {
+                update-props = {
+                  "session.suspend-timeout-seconds" = 0;
+                };
+
+              };
+
+            }
+          ];
+
+        };
+      };
+    };
   };
+
   #sound.enable = true;
 
   hardware = {
     pulseaudio.enable = lib.mkForce false;
   };
+
+  # Start WirePlumber (with PipeWire) at boot.
+  systemd.user.services.wireplumber.wantedBy = [ "default.target" ];
 
   environment.systemPackages = with pkgs; [
     pavucontrol

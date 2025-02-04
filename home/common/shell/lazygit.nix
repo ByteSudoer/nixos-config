f298742ce5033dfea39fee2d444082cf66da9076
ByteSudoer <Bytesudoer@no-reply.github.com>
2025-02-04 17:48:33 +0100

    Theme declaration

--- home/common/shell/lazygit.nix
+++ home/common/shell/lazygit.nix
@@ -1,13 +1,73 @@
-{ pkgs, ... }:
+{ pkgs, colorscheme, ... }:
+let
+  themeConfig =
+    if colorscheme == "dracula" then
+      {
+        activeBorderColor = [
+          "#FF79C6"
+          "bold"
+        ];
+        inactiveBorderColor = [
+          "#BD93F9"
+        ];
+        searchingActiveBorderColor = [
+          "#8BE9FD"
+          "bold"
+        ];
+        optionsTextColor = [
+          "#6272A4"
+        ];
+        selectedLineBgColor = [
+          "#6272A4"
+        ];
+        inactiveViewSelectedLineBgColor = [
+          "bold"
+        ];
+        cherryPickedCommitFgColor = [
+          "#6272A4"
+        ];
+        cherryPickedCommitBgColor = [
+          "#8BE9FD"
+        ];
+        markedBaseCommitFgColor = [
+          "#8BE9FD"
+        ];
+        markedBaseCommitBgColor = [
+          "#F1FA8C"
+        ];
+        unstagedChangesColor = [
+          "#FF5555"
+        ];
+        defaultFgColor = [
+          "#F8F8F2"
+        ];
+      }
+    else
+      {
+
+      };
+
+in
 {
   programs.lazygit = {
     enable = true;
     package = pkgs.lazygit;
     settings = {
+      gui = {
+        theme = themeConfig;
+      };
       git = {
         paging = {
           colorOrg = "always";
           useConfig = false;
+        };
+        merging = {
+          manualCommit = true;
+          squashMergeMessage = "Squash merge {{selectedRef}} into {{currentBranch}}";
+          mainBranches = [
+            "main"
+            "master"
+          ];
         };
 
       };

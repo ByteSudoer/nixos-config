e8ea1aadc431badee1f6a30fc0ffed3c0d63b28b
ByteSudoer <Bytesudoer@no-reply.github.com>
2025-02-26 11:19:05 +0100

    Install vmware

--- host/common/virt/default.nix
+++ host/common/virt/default.nix
@@ -3,7 +3,7 @@
   imports =
     [ ./docker.nix ] # Include quickemu if a desktop is defined
     ++ lib.optional (builtins.isString extra) ./quickemu.nix
-    # ++ lib.optional (builtins.isString extra) ./vmware.nix
+    ++ lib.optional (builtins.isString extra) ./vmware.nix
     # ++ lib.optional (builtins.isString extra) ./virtualbox.nix
     ++ lib.optional (builtins.isString extra) ./virt-manager.nix;
 

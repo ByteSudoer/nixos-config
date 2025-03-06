73293a5e12fe179e07e41654b5d186127af04639
ByteSudoer <Bytesudoer@no-reply.github.com>
2025-03-06 20:16:59 +0100

    Add PDF utilities

--- home/extra/libreoffice.nix
+++ home/extra/libreoffice.nix
@@ -5,5 +5,21 @@
     hunspell
     hunspellDicts.en_US
     hunspellDicts.fr-moderne
+
+    #Edit pdf files
+    pdfarranger
   ];
+
+  programs.okular = {
+    enable = true;
+    general = {
+      mouseMode = "TextSelect";
+      openFileInTabs = true;
+      showScrollbars = true;
+      smoothScrolling = true;
+      viewContinuous = true;
+      viewMode = "Facing";
+      zoomMode = "fitWidth";
+    };
+  };
 }

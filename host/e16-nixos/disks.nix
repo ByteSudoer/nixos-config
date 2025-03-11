31e06f5367c84e06cff6f3b094a7181fa62ac61a
ByteSudoer <Bytesudoer@no-reply.github.com>
2025-03-12 00:25:05 +0100

    Add Second SSD config: not enabled

--- host/e16-nixos/disks.nix
+++ host/e16-nixos/disks.nix
@@ -50,3 +50,87 @@
     };
   };
 }
+#
+# {
+#   disko.devices = {
+#     disk = {
+#       root_disk = {
+#         type = "disk";
+#         device = builtins.elemAt disks 1;
+#         content = {
+#           type = "gpt";
+#           partitions = {
+#             ESP = {
+#               priority = 1;
+#               name = "ESP";
+#               start = "1M";
+#               end = "512M";
+#               type = "EF00";
+#               content = {
+#                 type = "filesystem";
+#                 format = "vfat";
+#                 mountpoint = "/boot";
+#                 mountOptions = [ "umask=0077" ];
+#               };
+#             };
+#             root = {
+#               size = "100%";
+#               content = {
+#                 type = "btrfs";
+#                 extraArgs = [
+#                   "-L"
+#                   "nixos"
+#                   "-f"
+#                 ];
+#                 subvolumes = {
+#                   "/rootfs" = {
+#                     mountpoint = "/";
+#                   };
+#                   "/nix" = {
+#                     mountOptions = [
+#                       "compress=zstd"
+#                       "noatime"
+#                     ];
+#                     mountpoint = "/nix";
+#                   };
+#                   "/swap" = {
+#                     mountpoint = "/.swapvol";
+#                     swap = {
+#                       swapfile.size = "8G";
+#                     };
+#                   };
+#                 };
+#               };
+#             };
+#           };
+#         };
+#       };
+#
+#       home_disk = {
+#         type = "disk";
+#         device = builtins.elemAt disks 1;
+#         content = {
+#           type = "gpt";
+#           partitions = {
+#             home = {
+#               size = "100%";
+#               content = {
+#                 type = "btrfs";
+#                 extraArgs = [ "-f" ];
+#                 subvolumes = {
+#                   "/home" = {
+#                     mountOptions = [
+#                       "compress=zstd"
+#                       "noatime"
+#                     ];
+#                     mountpoint = "/home";
+#                   };
+#                 };
+#               };
+#             };
+#           };
+#         };
+#       };
+#     };
+#   };
+# }

5a1b356ee65c5d3969d87466380bccf797eb38e2
ByteSudoer <ByteSudoer@users.noreply.github.com>
2025-03-17 12:46:31 +0100

    Fix disk name

--- host/e16-nixos/disks.nix
+++ host/e16-nixos/disks.nix
@@ -5,61 +5,12 @@
   ],
   ...
 }:
-# {
-#   disko.devices = {
-#     disk = {
-#       nvme = {
-#         device = builtins.elemAt disks 0;
-#         type = "disk";
-#         content = {
-#           type = "gpt";
-#           partitions = {
-#             ESP = {
-#               size = "500M";
-#               type = "EF00";
-#               content = {
-#                 type = "filesystem";
-#                 format = "vfat";
-#                 mountpoint = "/boot";
-#               };
-#             };
-#             root = {
-#               end = "-5G";
-#               content = {
-#                 type = "filesystem";
-#                 format = "ext4";
-#                 mountpoint = "/";
-#               };
-#             };
-#             encryptedSwap = {
-#               size = "100M";
-#               content = {
-#                 type = "swap";
-#                 randomEncryption = true;
-#                 priority = 100; # prefer to encrypt as long as we have space for it
-#               };
-#             };
-#             plainSwap = {
-#               size = "100%";
-#               content = {
-#                 type = "swap";
-#                 discardPolicy = "both";
-#                 resumeDevice = true; # resume from hiberation from this device
-#               };
-#             };
-#           };
-#         };
-#       };
-#     };
-#   };
-# }
-#
 {
   disko.devices = {
     disk = {
       root_disk = {
         type = "disk";
-        device = builtins.elemAt disks 1;
+        device = builtins.elemAt disks 0;
         content = {
           type = "gpt";
           partitions = {

64034374fd6b31a664a2abbb74c35e4051050a63
Soussi Mohamed Nour <88513682+ByteSudoer@users.noreply.github.com>
2025-03-17 10:32:05 +0100

    Update disks.nix

--- host/e16-nixos/disks.nix
+++ host/e16-nixos/disks.nix
@@ -1,131 +1,50 @@
 {
-  disks ? [ "/dev/nvme0n1" ],
+  disks ? [
+    "/dev/nvme0n1"
+    "/dev/nvme1n1"
+  ],
   ...
 }:
-{
-  disko.devices = {
-    disk = {
-      nvme = {
-        device = builtins.elemAt disks 0;
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              size = "500M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
-            };
-            root = {
-              end = "-5G";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-              };
-            };
-            encryptedSwap = {
-              size = "100M";
-              content = {
-                type = "swap";
-                randomEncryption = true;
-                priority = 100; # prefer to encrypt as long as we have space for it
-              };
-            };
-            plainSwap = {
-              size = "100%";
-              content = {
-                type = "swap";
-                discardPolicy = "both";
-                resumeDevice = true; # resume from hiberation from this device
-              };
-            };
-          };
-        };
-      };
-    };
-  };
-}
-#
 # {
 #   disko.devices = {
 #     disk = {
-#       root_disk = {
+#       nvme = {
+#         device = builtins.elemAt disks 0;
 #         type = "disk";
-#         device = builtins.elemAt disks 1;
 #         content = {
 #           type = "gpt";
 #           partitions = {
 #             ESP = {
-#               priority = 1;
-#               name = "ESP";
-#               start = "1M";
-#               end = "512M";
+#               size = "500M";
 #               type = "EF00";
 #               content = {
 #                 type = "filesystem";
 #                 format = "vfat";
 #                 mountpoint = "/boot";
-#                 mountOptions = [ "umask=0077" ];
 #               };
 #             };
 #             root = {
-#               size = "100%";
+#               end = "-5G";
 #               content = {
-#                 type = "btrfs";
-#                 extraArgs = [
-#                   "-L"
-#                   "nixos"
-#                   "-f"
-#                 ];
-#                 subvolumes = {
-#                   "/rootfs" = {
-#                     mountpoint = "/";
-#                   };
-#                   "/nix" = {
-#                     mountOptions = [
-#                       "compress=zstd"
-#                       "noatime"
-#                     ];
-#                     mountpoint = "/nix";
-#                   };
-#                   "/swap" = {
-#                     mountpoint = "/.swapvol";
-#                     swap = {
-#                       swapfile.size = "8G";
-#                     };
-#                   };
-#                 };
+#                 type = "filesystem";
+#                 format = "ext4";
+#                 mountpoint = "/";
 #               };
 #             };
-#           };
-#         };
-#       };
-#
-#       home_disk = {
-#         type = "disk";
-#         device = builtins.elemAt disks 1;
-#         content = {
-#           type = "gpt";
-#           partitions = {
-#             home = {
+#             encryptedSwap = {
+#               size = "100M";
+#               content = {
+#                 type = "swap";
+#                 randomEncryption = true;
+#                 priority = 100; # prefer to encrypt as long as we have space for it
+#               };
+#             };
+#             plainSwap = {
 #               size = "100%";
 #               content = {
-#                 type = "btrfs";
-#                 extraArgs = [ "-f" ];
-#                 subvolumes = {
-#                   "/home" = {
-#                     mountOptions = [
-#                       "compress=zstd"
-#                       "noatime"
-#                     ];
-#                     mountpoint = "/home";
-#                   };
-#                 };
+#                 type = "swap";
+#                 discardPolicy = "both";
+#                 resumeDevice = true; # resume from hiberation from this device
 #               };
 #             };
 #           };
@@ -134,3 +53,87 @@
 #     };
 #   };
 # }
+#
+{
+  disko.devices = {
+    disk = {
+      root_disk = {
+        type = "disk";
+        device = builtins.elemAt disks 1;
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              priority = 1;
+              name = "ESP";
+              start = "1M";
+              end = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = [
+                  "-L"
+                  "nixos"
+                  "-f"
+                ];
+                subvolumes = {
+                  "/rootfs" = {
+                    mountpoint = "/";
+                  };
+                  "/nix" = {
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                    mountpoint = "/nix";
+                  };
+                  "/swap" = {
+                    mountpoint = "/.swapvol";
+                    swap = {
+                      swapfile.size = "8G";
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+
+      home_disk = {
+        type = "disk";
+        device = builtins.elemAt disks 1;
+        content = {
+          type = "gpt";
+          partitions = {
+            home = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ];
+                subvolumes = {
+                  "/home" = {
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                    mountpoint = "/home";
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}

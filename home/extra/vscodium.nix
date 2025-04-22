7f4f1a267498841fe05a433457857a96d8480f46
ByteSudoer <ByteSudoer@users.noreply.github.com>
2025-04-22 10:44:56 +0100

    user settings

--- home/extra/vscodium.nix
+++ home/extra/vscodium.nix
@@ -6,6 +6,7 @@ in
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
+    mutableExtensionsDir = true;
     extensions = with pkgs.vscode-extensions; [
 
       #Nix support
@@ -27,9 +28,10 @@ in
       ms-python.python
       ms-python.black-formatter
       charliermarsh.ruff
+      #james-yu.latex-workshop
 
       #Spell Checker
-      streetsidesoftware.code-spell-checker
+      #streetsidesoftware.code-spell-checker
 
       #Icons
       vscode-icons-team.vscode-icons
@@ -38,99 +40,99 @@ in
       github.vscode-github-actions
 
     ];
-    # userSettings = {
-    #   "files.autoSave" = "onFocusChange";
-    #   "[nix]"."editor.tabSize" = 2;
-    #   "telemetry.telemetryLevel" = "off";
-    #   "redhat.telemetry.enabled" = false;
-    #
-    #   "workbench" = {
-    #     "colorTheme" = "${ColorScheme}";
-    #     "iconTheme" = "vscode-icons";
-    #     "list" = {
-    #       "smoothScrolling" = true;
-    #     };
-    #   };
-    #
-    #   "editor" = {
-    #     "formatOnSave" = true;
-    #     "smoothScrolling" = true;
-    #     "cursorBlinking" = "smooth";
-    #     "cursorSmoothCaretAnnimation" = "on";
-    #   };
-    #
-    #   #Terraform Formatting
-    #   "[terraform]" = {
-    #     "editor.defaultFormatter" = "hashicorp.terraform";
-    #     "editor.formatOnSave" = false;
-    #     "editor.codeActionsOnSave" = {
-    #       "source.formatAll.terraform" = true;
-    #     };
-    #   };
-    #   "[terraform-vars]" = {
-    #     "editor.defaultFormatter" = "hashicorp.terraform";
-    #     "editor.formatOnSave" = false;
-    #     "editor.codeActionsOnSave" = {
-    #       "source.formatAll.terraform" = true;
-    #     };
-    #   };
-    #   "terraform" = {
-    #     "experimentalFeatures.prefillRequiredFields" = true;
-    #     "experimentalFeatures.validateOnSave" = true;
-    #     "editor.suggest.preview" = true;
-    #   };
-    #
-    #   #Python & Notebook linting and Formatting
-    #   "[python]" = {
-    #     "editor.formatOnSave" = true;
-    #     "editor.codeActionsOnSave" = {
-    #       "source.fixAll" = "explicit";
-    #       "source.organizeImports" = true;
-    #     };
-    #     "editor.defaultFormatter" = "charliermarsh.ruff";
-    #   };
-    #
-    #   "notebook.formatOnSave.enabled" = true;
-    #   "notebook.codeActionsOnSave" = {
-    #     "notebook.source.fixAll" = "explicit";
-    #     "notebook.source.organizeImports" = "explicit";
-    #   };
-    #
-    #   # Spell Checker Configuration
-    #   "cSpell.enabled" = true;
-    #   "cSpell.language" = "en,fr";
-    #   "cSpell.diagnosticLevel" = "Information";
-    #   "cSpell.maxNumberOfProblems" = 100;
-    #   "cSpell.numSuggestions" = 8;
-    #   "cSpell.minWordLength" = 4;
-    #   "cSpell.enabledFileTypes" = [
-    #     "bash"
-    #     "c"
-    #     "csharp"
-    #     "go"
-    #     "javascript"
-    #     "javascriptreact"
-    #     "markdown"
-    #     "php"
-    #     "plaintext"
-    #     "python"
-    #     "rust"
-    #     "sql"
-    #     "typescript"
-    #     "typescriptreact"
-    #     "yaml"
-    #   ];
-    #   "cSpell.ignorePaths" = [
-    #     "node_modules"
-    #     "**/node_modules"
-    #     "**/node_modules/**"
-    #     "node_modules/**"
-    #     "vscode-extension"
-    #     ".git"
-    #     "*.dll"
-    #     "**/*.dll"
-    #   ];
-    # };
+    userSettings = {
+      "files.autoSave" = "onFocusChange";
+      "[nix]"."editor.tabSize" = 2;
+      "telemetry.telemetryLevel" = "off";
+      "redhat.telemetry.enabled" = false;
+
+      "workbench" = {
+        "colorTheme" = "${ColorScheme}";
+        "iconTheme" = "vscode-icons";
+        "list" = {
+          "smoothScrolling" = true;
+        };
+      };
+
+      "editor" = {
+        "formatOnSave" = true;
+        "smoothScrolling" = true;
+        "cursorBlinking" = "smooth";
+        "cursorSmoothCaretAnnimation" = "on";
+      };
+
+      #Terraform Formatting
+      "[terraform]" = {
+        "editor.defaultFormatter" = "hashicorp.terraform";
+        "editor.formatOnSave" = false;
+        "editor.codeActionsOnSave" = {
+          "source.formatAll.terraform" = true;
+        };
+      };
+      "[terraform-vars]" = {
+        "editor.defaultFormatter" = "hashicorp.terraform";
+        "editor.formatOnSave" = false;
+        "editor.codeActionsOnSave" = {
+          "source.formatAll.terraform" = true;
+        };
+      };
+      "terraform" = {
+        "experimentalFeatures.prefillRequiredFields" = true;
+        "experimentalFeatures.validateOnSave" = true;
+        "editor.suggest.preview" = true;
+      };
+
+      #Python & Notebook linting and Formatting
+      "[python]" = {
+        "editor.formatOnSave" = true;
+        "editor.codeActionsOnSave" = {
+          "source.fixAll" = "explicit";
+          "source.organizeImports" = true;
+        };
+        "editor.defaultFormatter" = "charliermarsh.ruff";
+      };
+
+      "notebook.formatOnSave.enabled" = true;
+      "notebook.codeActionsOnSave" = {
+        "notebook.source.fixAll" = "explicit";
+        "notebook.source.organizeImports" = "explicit";
+      };
+
+      # Spell Checker Configuration
+      "cSpell.enabled" = true;
+      "cSpell.language" = "en,fr";
+      "cSpell.diagnosticLevel" = "Information";
+      "cSpell.maxNumberOfProblems" = 100;
+      "cSpell.numSuggestions" = 8;
+      "cSpell.minWordLength" = 4;
+      "cSpell.enabledFileTypes" = {
+        "bash" = "true";
+        "c" = "true";
+        "csharp" = "true";
+        "go" = "true";
+        "javascript" = "true";
+        "javascriptreact" = "true";
+        "markdown" = "true";
+        "php" = "true";
+        "plaintext" = "true";
+        "python" = "true";
+        "rust" = "true";
+        "sql" = "true";
+        "typescript" = "true";
+        "typescriptreact" = "true";
+        "yaml" = "true";
+      };
+      "cSpell.ignorePaths" = [
+        "node_modules"
+        "**/node_modules"
+        "**/node_modules/**"
+        "node_modules/**"
+        "vscode-extension"
+        ".git"
+        "*.dll"
+        "**/*.dll"
+      ];
+    };
 
   };
 }

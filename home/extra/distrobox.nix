646d792e8b73c363acb81b27a2965f6cb39baf80
ByteSudoer <ByteSudoer@users.noreply.github.com>
2025-05-21 10:03:46 +0100

    use module for distrobox

--- home/extra/distrobox.nix
+++ home/extra/distrobox.nix
@@ -1,4 +1,20 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs.unstable; [ distrobox ];
+
+  home.packages = with pkgs; [ distrobox ];
+  # programs.distrobox = {
+  #   enable = true;
+  #   package = pkgs.distrobox;
+  #   enableSystemdUnit = false;
+  #
+  #   debian-dev = {
+  #     image = "debian:13";
+  #     entry = true;
+  #     additional_packages = "bash-completion git vim";
+  #     init_hooks = [
+  #       "ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/docker"
+  #       "ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/docker-compose"
+  #     ];
+  #   };
+  # };
 }
--- host/common/virt/docker.nix
+++ host/common/virt/docker.nix
@@ -8,7 +8,6 @@
 
   environment.systemPackages = with pkgs; [
     docker-compose
-    distrobox
     ctop
   ];
 }

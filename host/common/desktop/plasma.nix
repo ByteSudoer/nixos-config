12e561d6115ffa56bbe2106b3ef20ee437cff5b4
ByteSudoer <ByteSudoer@users.noreply.github.com>
2025-06-08 18:56:55 +0100

    Fix

--- host/common/desktop/plasma.nix
+++ host/common/desktop/plasma.nix
@@ -6,8 +6,8 @@
   # Enable the KDE Plasma Desktop Environment.
   services.desktopManager.plasma6.enable = true;
   environment.plasma6.excludePackages = with pkgs; [
-    kate
-    plasma-browser-integration
+    kdePackages.kate
+    kdePackages.plasma-browser-integration
 
   ];
 }

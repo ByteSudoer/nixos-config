ba475b535e0e2030f4719206e6ba69a329afeb43
ByteSudoer <ByteSudoer@users.noreply.github.com>
2025-07-17 09:20:28 +0100

    added docker prune systemd unit

--- host/common/virt/docker.nix
+++ host/common/virt/docker.nix
@@ -3,6 +3,12 @@
   virtualisation = {
     docker = {
       enable = true;
+      package = pkgs.docker;
+      autoPrune = {
+        enable = true; # periodically prune Docker resources.
+        dates = "weekly"; # Specification (in the format described by systemd.time(7)) of the time at which the prune will occur.
+
+      };
     };
   };
 

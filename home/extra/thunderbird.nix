3e8327d1a3befa93bc5c57c7073820a43d011eeb
ByteSudoer <ByteSudoer@users.noreply.github.com>
2025-10-17 09:43:00 +0100

    disable thunderbird

--- home/extra/thunderbird.nix
+++ home/extra/thunderbird.nix
@@ -2,18 +2,58 @@
 {
   programs.thunderbird = {
     enable = false;
+    package = pkgs.thunderbird;
     profiles = {
-      # "${username}" = {
-      #   name = "ByteSudoer";
-      #   accountsOrder = [
-      #     "work"
-      #     "uni"
-      #   ];
-      # };
-    };
-    settings = {
-      "privacy.donottrackheader.enabled" = true;
+      "${username}" = {
+        isDefault = true;
+        #name = "ByteSudoer";
+        accountsOrder = [
+          "work"
+          "uni"
+        ];
+
+        settings = {
+          "privacy.donottrackheader.enabled" = true;
+          "mail.spellcheck.inline" = true;
+          "general.smoothScroll" = true;
+          # Auto enable extensions
+          "extensions.autoDisableScopes" = 0;
+        };
+
+        # Search engine customization (optional)
+        search = {
+          force = true;
+          default = "ddg";
+          engines = {
+            ddg = {
+              name = "DuckDuckGo";
+              urls = [ { template = "https://duckduckgo.com/?q={searchTerms}"; } ];
+            };
+            nixos = {
+              name = "Nix Packages";
+              urls = [
+                {
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+            };
+          };
+          order = [
+            "ddg"
+            "nixos"
+          ];
+        };
+      };
+
     };
 
   };
+
+  #home.packages = with pkgs; [ birdtray ];
 }
